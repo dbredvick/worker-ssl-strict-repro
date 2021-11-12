fa1328d24dac8f58cbde796d0833ed72863e10d7
Drew Bredvick <dbredvick@gmail.com>
2021-11-11 21:02:30 -0600

    fix: back to revue

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,13 +3,12 @@ import { NextResponse } from "next/server";
 async function redirects(req) {
   const { pathname } = req.nextUrl;
   if (pathname.includes("/hello-world")) {
-    const res = await fetch(`https://vercel.com`, {
+    const res = await fetch(`https://www.getrevue.co/api/v2/issues/latest`, {
       method: "GET",
-      //   headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
+      headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
     });
-    // const data = await res.json();
-    console.log(res);
-    return NextResponse.redirect("/");
+    const data = await res.json();
+    return NextResponse.redirect(data.issue[0].url);
   }
 }
 
