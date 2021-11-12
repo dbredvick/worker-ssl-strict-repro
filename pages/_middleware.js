d508d4d3c8dabdf6f3112ba4c4f7d6f8b4ee222d
Drew Bredvick <dbredvick@gmail.com>
2021-11-11 20:54:00 -0600

    fix: remove revue, switch to vercel

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,12 +3,13 @@ import { NextResponse } from "next/server";
 async function redirects(req) {
   const { pathname } = req.nextUrl;
   if (pathname.includes("/hello-world")) {
-    const res = await fetch(`https://www.getrevue.co/api/v2/issues/latest`, {
+    const res = await fetch(`https://vercel.com`, {
       method: "GET",
-      headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
+      //   headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
     });
-    const data = await res.json();
-    return NextResponse.redirect(data.issue[0].url);
+    // const data = await res.json();
+    console.log(res);
+    return NextResponse.redirect("/");
   }
 }
 
