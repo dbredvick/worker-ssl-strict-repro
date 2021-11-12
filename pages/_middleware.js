9574bd18cfaaa747869e077fc88539e41a516b97
Drew Bredvick <dbredvick@gmail.com>
2021-11-11 21:17:17 -0600

    fix: Peters site

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 async function redirects(req) {
   const { pathname } = req.nextUrl;
   if (pathname.includes("/hello-world")) {
-    const res = await fetch(`https://www.getrevue.co/api/v2/issues/latest`, {
+    const res = await fetch(`https://cfobj.peteryoakum.com/`, {
       method: "GET",
-      headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
+      //   headers: { Authorization: `Bearer ${process.env.REVUE_API}` },
     });
     console.log(res);
-    const data = await res.json();
-    return NextResponse.redirect(data.issue[0].url);
+    // const data = await res.json();
+    return NextResponse.redirect("/");
   }
 }
 
